a2ecbd27f1ab1bae3c6c00e52a001149e123a2af
Ugo Leone Cavalcanti <ugoleone.cavalcanti@gmail.com>
2023-04-16 22:43:26 +0200

    Add files via upload

--- iosaWeb/assets/js/main.js
+++ iosaWeb/assets/js/main.js
@@ -12,3 +12,16 @@ window.addEventListener("scroll", event => {
     var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
     output.scrollLeft = scrollTop;
     }, { passive: true });
+
+
+function toggleSubMenu(index) {
+    var subMenus = document.getElementsByClassName('subMenu');
+    previouslyOpen = subMenus[index].style.display == "flex"
+    for (i = 0; i < subMenus.length; i++)
+        subMenus[i].style.display = "none";
+    console.log("aho")
+    
+    if (index >= 0 && index < subMenus.length)
+        if(!previouslyOpen)
+            subMenus[index].style.display = "flex";
+}
